dcdaf4a1bada2b6ebcec039b33d383cd86eb7324
Krishan Arya <krishanmarya1@gmail.com>
2016-09-21 20:50:01 +1000

    Part database

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const db_host = 'localhost';
-const db_user = 'root';
-const db_password = '12345';
-const db_name = 'tbd';
+const db_host = 'ec2-50-17-227-146.compute-1.amazonaws.com';
+const db_user = 'nyequiyuwwajvb';
+const db_password = 'IjaYhRDw_jDRmKZyKDqvu4cp-q';
+const db_name = 'd4enm3kji12t97';
 
 const sequelize = new Sequelize(db_name, db_user, db_password, {
   host: db_host,
-  dialect: 'mysql'
+  dialect: 'postgres' /*'mysql'*/
 });
 
 module.exports = {
