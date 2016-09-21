8a131c70b5bd9460d045736d7ef65be48e49e981
Krishan Arya <krishanmarya1@gmail.com>
2016-09-21 20:13:19 +1000

    Change require statement

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const { db_host, db_user, db_password, db_name } = process.env;
+
+const db_host = process.env.db_host;
+const db_user = process.env.db_user;
+const db_password = process.env.db_password;
+const db_name = process.env.db_name;
 
 const sequelize = new Sequelize(db_name, db_user, db_password, {
   host: db_host,
